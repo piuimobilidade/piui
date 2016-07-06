f178db7e7fe01dc75e09220f44479e9989d86dc9
adilson.krasovski <adilson.krasovski@TANSC150.tansc.tan.com.br>
2016-07-06 13:52:15 -0300

    Alterado o nome do contexto, removido a versão

--- piui-api/src/main/java/co/piui/api/PiuiApplication.java
+++ piui-api/src/main/java/co/piui/api/PiuiApplication.java
@@ -3,7 +3,7 @@ package co.piui.api;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath( "api/v1.0" )
+@ApplicationPath( "api" )
 public class PiuiApplication extends Application {
 
 }
